abfcdd3899809d58892faa981f669a6e05735457
JosephTing91 <joeksting@gmail.com>
2023-01-05 16:36:22 -0700

    add code

---
+++ jenkins_userdata.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# Installing Jenkins
+sudo yum update –y
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum upgrade
+sudo amazon-linux-extras install java-openjdk11 -y
+sudo yum install jenkins -y
+sudo echo "jenkins ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
+# Installing Git
+sudo yum install git -y
+
+
+# Installing maven - commented out as usage of tools explanation is required.
+sudo wget https://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+sudo yum install -y apache-maven
+
+# node-exporter installations
+sudo useradd --no-create-home node_exporter
+
+wget https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz
+tar xzf node_exporter-1.0.1.linux-amd64.tar.gz
+sudo cp node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin/node_exporter
+rm -rf node_exporter-1.0.1.linux-amd64.tar.gz node_exporter-1.0.1.linux-amd64
+
+# setup the node-exporter dependencies
+sudo yum install git -y
+sudo git clone -b installations https://github.com/cvamsikrishna11/devops-fully-automated.git /tmp/devops-fully-automated
+sudo cp /tmp/devops-fully-automated/prometheus-setup-dependencies/node-exporter.service /etc/systemd/system/node-exporter.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable node-exporter
+sudo systemctl start node-exporter
+sudo systemctl status node-exporter
+
+# Setup terraform
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
+
+#for amazon linux 2
+sudo yum install docker -y
+#Add group membership for the default ec2-user so you can run all docker commands without using the sudo command:
+sudo usermod -a -G docker jenkins
+id jenkins
+# Reload a Linux user's group assignments to docker w/o logout
+newgrp docker
+
+#docker compose
+sudo yum install python3-pip
+sudo pip3 install docker-compose # with root access
+
+#enable docker service at boot
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
